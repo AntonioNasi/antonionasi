ffa9968c69a6d21cd5f5266d5fcba897a76d4e79
Toim Nasi <toimnasi@gmail.com>
2026-02-24 17:35:18 -0300

    fix_PyCode

--- .github/scripts/post_to_facebook.py
+++ .github/scripts/post_to_facebook.py
@@ -1,7 +1,11 @@
 import requests
 import xml.etree.ElementTree as ET
+import os
 
 FEED_URL = "https://antonionasi.com.br/feed.xml"
+PAGE_ID = os.getenv("FACEBOOK_PAGE_ID")
+PAGE_TOKEN = os.getenv("FACEBOOK_PAGE_TOKEN")
+GRAPH_VERSION = "v18.0"
 
 def get_latest_post():
     response = requests.get(FEED_URL)
@@ -18,3 +22,19 @@ def get_latest_post():
     link = entry.find('atom:link', namespace).attrib['href']
 
     return title, link
+
+def publish_post(title, link):
+    url = f"https://graph.facebook.com/{GRAPH_VERSION}/{PAGE_ID}/feed"
+
+    payload = {
+        "message": f"{title}\n\nLeia mais:",
+        "link": link,
+        "access_token": PAGE_TOKEN
+    }
+
+    response = requests.post(url, data=payload)
+    print(response.json())
+
+if __name__ == "__main__":
+    title, link = get_latest_post()
+    publish_post(title, link)
